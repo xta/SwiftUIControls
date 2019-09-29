6486e17376df3f2ca1b67b526627df203140cab7
Rex Feng <rexfeng@gmail.com>
2019-09-28 23:52:20 -0700

    Initial Commit

---
+++ SwiftUIControls/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIControls
+//
+//  Created by Rex Feng on 9/28/19.
+//  Copyright © 2019 xta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
